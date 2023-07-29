b06cd63498ce7ea72594e8e7826b2b19c2be02c2
mariamrevishvili <140517621+mariamrevishvili@users.noreply.github.com>
2023-07-30 01:02:20 +0400

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,4 @@
+user_age=input("how old are you?")
+futuretime=input("how many years do you want to time travel?")
+age_after_time_travel=user_age +futuretime
+print("after time travel,you will be", age_after_time_travel, "years old.")
